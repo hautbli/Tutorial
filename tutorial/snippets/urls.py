f4e750e6426e5b01dc15e70bad6c747abcfd5e86
withHappy <hap2.dev@gmail.com>
2020-06-08 00:51:59 +0900

    drf-tutorial

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,59 @@
+from django.urls import path, include
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets.views import SnippetViewSet, UserViewSet
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+
+# 라우터 사용
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+#
+# # viewset 사용
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+#
+# urlpatterns = format_suffix_patterns([
+#     # path('', api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail')
+# ])
+# urlpatterns = [
+#     # FBV
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail),
+#     # CBV
+#     path('snippets/', views.SnippetList.as_view()),
+#     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+#     path('users/', views.UserList.as_view()),
+#     path('users/<int:pk>/', views.UserDetail.as_view()),
+#     path('api-auth/', include('rest_framework.urls')),
+#
+# ]
+# urlpatterns = format_suffix_patterns(urlpatterns)
